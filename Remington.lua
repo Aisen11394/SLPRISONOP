aef79013687b016d280c5f7933ef0ccf57b27dd7
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-10-06 10:48:14 +0900

    Create Remington.lua

---
+++ Remington.lua
@@ -0,0 +1,8 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrane.new(1169.7, 25.5, 618.5)
+wait(0.05)
+local args = {
+    [1] = "Remington 870",
+    [2] = workspace.ClickInteract.Interact
+}
+
+game:GetService("ReplicatedStorage").Events.EquipTool:FireServer(unpack(args))
