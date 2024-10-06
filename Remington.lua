425f7f1e82dde80c82b46e6f1d06d3bcde045ed5
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-10-06 10:58:53 +0900

    Update Remington.lua

--- Remington.lua
+++ Remington.lua
@@ -1,4 +1,4 @@
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrane.new(1169.7, 25.5, 618.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrane.new(1169.7, 25.5, 618.5) wait()
 wait(0.05)
 local args = {
     [1] = "Remington 870",
